8a7f40550d1ffb9b14c1cda204efee5b7371b246
seo2im <seo2im6492@gmail.com>
2021-05-20 13:12:43 +0900

    chore:set default environment setting - set test library(jest) - set linting env(eslint) 	- prettier & style guide update later - set build system 	- webpack, awsome-ts-loader, @types

---
+++ srcs/index.tsx
@@ -0,0 +1,11 @@
+/*
+import React from 'react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { ContextMenu } from '../srcs/Components'
+
+describe("<ContextMenu />", () => {
+    test("should?", async () => {
+        
+    })
+})
+*/
